3e7eb39c2080a73175d7d5c053968caba0df5932
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-09-22 01:38:16 -0300

    sou brasileiro

--- build.sh
+++ build.sh
@@ -5,11 +5,6 @@ echo "Building bombonato.net"
 
 gem install bundler
 bundle install
-gem install jekyll
-gem install jekyll-sitemap
-gem install jekyll-feed
-gem install github-pages
-
 bundle exec jekyll build
 
 echo "Finished"
