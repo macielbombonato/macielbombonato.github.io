025239c0def4ef011117b363019fc69574bb39f6
Maciel Escudero Bombonato <mbombonato@salesforce.com>
2026-06-02 17:30:32 -0300

    Update mcp-related-renderer.js

--- assets/js/mcp-related-renderer.js
+++ assets/js/mcp-related-renderer.js
@@ -349,7 +349,7 @@
         if (document.readyState === "loading") {
             document.addEventListener("DOMContentLoaded", fn);
         } else {
-            setTimeout(fn, 0);
+            setTimeout(fn, 5000);
         }
     }
 
